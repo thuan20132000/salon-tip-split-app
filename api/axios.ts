bd07f3ca4798747e7fc211f87f599eba0325084f
thuantm <thuantm@flydinotech.com>
2025-01-27 04:46:30 -0500

    - update api

--- api/axios.ts
+++ api/axios.ts
@@ -3,9 +3,14 @@ import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { AuthResponse, APIError } from '../types/api.types';
 import * as SecureStore from 'expo-secure-store';
 
+// Local
 // const API_URL = "http://192.168.2.226:8000/api/";
-// const API_URL = "https://salon-tipsplit-dev.thuandev.site/api/";
-const API_URL = "https://salon-tipsplit-dev-v2.thuandev.site/api/";
+
+// Development
+const API_URL = "https://salon-tipsplit-dev.thuandev.site/api/";
+
+// Production
+// const API_URL = "https://salon-tipsplit-dev-v2.thuandev.site/api/";
 
 class APIClient {
   private static instance: APIClient;
